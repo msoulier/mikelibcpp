7c74cc95f8b810f5fd7ee6e07508798433d8ecee
Michael P. Soulier <msoulier@digitaltorque.ca>
2021-12-05 21:20:04 -0500

    Removing simple logger and making tls_buffer logger object specific.

--- mlogger.cpp
+++ mlogger.cpp
@@ -7,119 +7,18 @@
 #include "mlogger.hpp"
 
 //////////////////////////////////////////////////////////////////////
-// SimpleLogger - A first pass at a thread-safe logger.             //
-//////////////////////////////////////////////////////////////////////
-
-SimpleLogger::SimpleLogger() : m_level(LOGLEVEL_INFO), m_ostream(std::cerr)
-{
-
-}
-
-SimpleLogger::~SimpleLogger() {
-    
-}
-
-void SimpleLogger::setLevel(int level) {
-    m_level = level;
-}
-
-int SimpleLogger::getLevel() {
-    return m_level;
-}
-
-const std::string SimpleLogger::localDateTime() {
-    const char *format = "%b %d %Y @ %X %Z";
-    std::time_t t = std::time(NULL);
-    char buffer[128];
-    if (std::strftime(buffer, sizeof(buffer), format, std::localtime(&t))) {
-        return std::string(buffer);
-    }
-    else {
-        return "";
-    }
-}
-
-void SimpleLogger::log(int level, std::string msg) {
-    if (level >= m_level) {
-        m_mutex.lock();
-        switch (level) {
-            case LOGLEVEL_TRACE:
-                m_ostream << "[TRACE] ";
-                break;
-            case LOGLEVEL_DEBUG:
-                m_ostream << "[DEBUG] ";
-                break;
-            case LOGLEVEL_INFO:
-                m_ostream << "[INFO] ";
-                break;
-            case LOGLEVEL_WARN:
-                m_ostream << "[WARNING] ";
-                break;
-            case LOGLEVEL_ERROR:
-                m_ostream << "[ERROR] ";
-                break;
-            default:
-                m_ostream << "[" << level << "] ";
-        }
-        m_ostream << localDateTime() << " ";
-        m_ostream << msg << std::endl;
-        m_mutex.unlock();
-    }
-}
-
-void SimpleLogger::trace(const char *msg) {
-    this->log(LOGLEVEL_TRACE, std::string(msg));
-}
-
-void SimpleLogger::trace(std::string msg) {
-    this->log(LOGLEVEL_TRACE, msg);
-}
-
-void SimpleLogger::debug(const char *msg) {
-    this->log(LOGLEVEL_DEBUG, std::string(msg));
-}
-
-void SimpleLogger::debug(std::string msg) {
-    this->log(LOGLEVEL_DEBUG, msg);
-}
-
-void SimpleLogger::info(const char *msg) {
-    this->log(LOGLEVEL_INFO, std::string(msg));
-}
-
-void SimpleLogger::info(std::string msg) {
-    this->log(LOGLEVEL_INFO, msg);
-}
-
-void SimpleLogger::warn(const char *msg) {
-    this->log(LOGLEVEL_WARN, std::string(msg));
-}
-
-void SimpleLogger::warn(std::string msg) {
-    this->log(LOGLEVEL_WARN, msg);
-}
-
-void SimpleLogger::error(const char *msg) {
-    this->log(LOGLEVEL_ERROR, std::string(msg));
-}
-
-void SimpleLogger::error(std::string msg) {
-    this->log(LOGLEVEL_ERROR, msg);
-}
-
-//////////////////////////////////////////////////////////////////////
 // MLogger - A second pass at a C++, thread-safe logger.            //
 //     Note that it returns MLoggerHandler objects to do the actual //
 //     logging.                                                     //
 //////////////////////////////////////////////////////////////////////
 
-boost::thread_specific_ptr<std::stringstream> tls_buffer;
-
-MLoggerHandler::MLoggerHandler(boost::mutex& mutex,
+MLoggerHandler::MLoggerHandler(boost::thread_specific_ptr<std::stringstream>& buffer,
+                               boost::mutex& mutex,
                                std::ostream& ostream,
                                int threshold,
                                std::string prefix)
-    : m_mutex(mutex)
+    : m_buffer(buffer)
+    , m_mutex(mutex)
     , m_level(LOGLEVEL_INFO)
     , m_ostream(ostream)
     , m_threshold(threshold)
@@ -148,11 +47,11 @@ MLogger::MLogger(std::string name)
     : m_name(name)
     , m_level(LOGLEVEL_INFO)
     , m_ostream(std::cerr)
-    , m_trace_handler(MLoggerHandler(m_mutex, m_ostream, LOGLEVEL_TRACE, "TRACE"))
-    , m_debug_handler(MLoggerHandler(m_mutex, m_ostream, LOGLEVEL_DEBUG, "DEBUG"))
-    , m_info_handler(MLoggerHandler(m_mutex, m_ostream, LOGLEVEL_INFO, "INFO"))
-    , m_warn_handler(MLoggerHandler(m_mutex, m_ostream, LOGLEVEL_WARN, "WARN"))
-    , m_error_handler(MLoggerHandler(m_mutex, m_ostream, LOGLEVEL_ERROR, "ERROR"))
+    , m_trace_handler(MLoggerHandler(m_buffer, m_mutex, m_ostream, LOGLEVEL_TRACE, "TRACE"))
+    , m_debug_handler(MLoggerHandler(m_buffer, m_mutex, m_ostream, LOGLEVEL_DEBUG, "DEBUG"))
+    , m_info_handler(MLoggerHandler(m_buffer, m_mutex, m_ostream, LOGLEVEL_INFO, "INFO"))
+    , m_warn_handler(MLoggerHandler(m_buffer, m_mutex, m_ostream, LOGLEVEL_WARN, "WARN"))
+    , m_error_handler(MLoggerHandler(m_buffer, m_mutex, m_ostream, LOGLEVEL_ERROR, "ERROR"))
     { }
 
 MLogger::MLogger() : MLogger("No name") { }
--- mlogger.hpp
+++ mlogger.hpp
@@ -21,58 +21,16 @@
 #define LOGLEVEL_ERROR 40
 
 //////////////////////////////////////////////////////////////////////
-// SimpleLogger - A first pass at a thread-safe logger.             //
-//////////////////////////////////////////////////////////////////////
-
-class SimpleLogger
-{
-public:
-    SimpleLogger();
-    ~SimpleLogger();
-    // Set the current logging level
-    void setLevel(int level);
-    // Get the current logging level
-    int getLevel();
-    // Convenience methods for trace level log.
-    void trace(const char *msg);
-    void trace(std::string msg);
-    // Convenience methods for debug level log
-    void debug(const char *msg);
-    void debug(std::string msg);
-    // Convenience methods for info level log
-    void info(const char *msg);
-    void info(std::string msg);
-    // Convenience methods for warning level log
-    void warn(const char *msg);
-    void warn(std::string msg);
-    // Convenicence methods for error level log
-    void error(const char *msg);
-    void error(std::string msg);
-private:
-    // The current log level.
-    int m_level;
-    // The mutex used for synchronization.
-    boost::mutex m_mutex;
-    // The output stream for the logger. FIXME: implement logging handlers
-    std::ostream& m_ostream;
-    // The actual main logging function.
-    void log(int level, std::string msg);
-    // Return the current date and time as a localized string.
-    const std::string localDateTime();
-};
-
-//////////////////////////////////////////////////////////////////////
 // MLogger - A second pass at a C++, thread-safe logger.            //
 //     Note that it returns MLoggerHandler objects to do the actual //
 //     logging.                                                     //
 //////////////////////////////////////////////////////////////////////
 
-extern boost::thread_specific_ptr<std::stringstream> tls_buffer;
-
 class MLoggerHandler
 {
 public:
-    MLoggerHandler(boost::mutex& mutex,
+    MLoggerHandler(boost::thread_specific_ptr<std::stringstream>& buffer,
+                   boost::mutex& mutex,
                    std::ostream& ostream,
                    int threshold,
                    std::string prefix);
@@ -84,14 +42,14 @@ public:
     MLoggerHandler& operator <<(T input) {
         // Only log if the level is set above our threshold.
         if (m_threshold >= m_level) {
-            if (tls_buffer.get() == NULL) {
-                tls_buffer.reset(new std::stringstream());
+            if (m_buffer.get() == NULL) {
+                m_buffer.reset(new std::stringstream());
             }
-            if (tls_buffer->str().length() == 0) {
-                *tls_buffer << localDateTime() << " " << m_prefix << ": " << input;
+            if (m_buffer->str().length() == 0) {
+                *m_buffer << localDateTime() << " " << m_prefix << ": " << input;
             }
             else {
-                *tls_buffer << input;
+                *m_buffer << input;
             }
         }
         return *this;
@@ -102,14 +60,16 @@ public:
         if (m_threshold >= m_level) {
             boost::lock_guard<boost::mutex> lock{m_mutex};
             // Flush the buffer
-            m_ostream << tls_buffer->str();
+            m_ostream << m_buffer->str();
             f(m_ostream);
             // Clear the buffer
-            tls_buffer->str("");
+            m_buffer->str("");
         }
         return m_ostream;
     }
 private:
+    // The thread-safe buffer where the logs are composed.
+    boost::thread_specific_ptr<std::stringstream>& m_buffer;
     // A mutex passed in from the main logger for synchronization.
     boost::mutex& m_mutex;
     // The logging level.
@@ -169,6 +129,8 @@ private:
     MLoggerHandler m_warn_handler;
     // Error handler
     MLoggerHandler m_error_handler;
+    // The thread-safe buffer where the logs are composed.
+    boost::thread_specific_ptr<std::stringstream> m_buffer;
 };
 
 extern MLogger logger;
